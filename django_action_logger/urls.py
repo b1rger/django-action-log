afcbb8869daeee6ed9c5957692067cec40934de0
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-19 10:52:25 +0100

    fix: rename templates and templatetag to `django_action_logGER`

--- django_action_logger/urls.py
+++ django_action_logger/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name = "django_action_log"
+app_name = "django_action_logger"
 
 urlpatterns = [
     path(
         "log",
         views.Log.as_view(),
-        name="djangoactionlog",
+        name="log",
     ),
 ]
