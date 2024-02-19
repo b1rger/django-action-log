eaa701f534414c6feacafef145aea842586d5702
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-19 10:17:16 +0100

    fix: update app name to `django_action_logger`

--- django_action_logger/apps.py
+++ django_action_logger/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class DjangoActionLogLogConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
-    name = "django_action_log"
+    name = "django_action_logger"
 
     def ready(self):
         from . import signals
